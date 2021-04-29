98ad39fc60a099d5bbe043e1656467a24034a044
sararamadanshater <saraelshater5555@gmail.com>
2021-04-29 15:07:47 +0200

    user auth /social

---
+++ social/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    body=forms.CharField(label='',widget=forms.Textarea(attrs={'rows':'3','placeholder':'say some thing'}))
+    class Meta:
+        
+        model=Post
+        fields=['body']
