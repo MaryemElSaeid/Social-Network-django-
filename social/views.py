4203e5e4594623b8b9de4293d6f6bf8bc063dac4
toka99 <toka.attiah@gamil.com>
2021-04-30 19:15:38 +0200

    add friend/remove friend/friends list toka mariam rewan

--- social/views.py
+++ social/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
@@ -16,6 +16,7 @@ class PostListView(LoginRequiredMixin, View):
             'post_list': posts,
             'form': form,
         }
+
         return render(request, 'social/post_list.html', context)
 
     def post(self, request, *args, **kwargs):
@@ -31,12 +32,14 @@ class PostListView(LoginRequiredMixin, View):
             'post_list': posts,
             'form': form,
         }
+
         return render(request, 'social/post_list.html', context)
 
 class PostDetailView(LoginRequiredMixin, View):
     def get(self, request, pk, *args, **kwargs):
         post = Post.objects.get(pk=pk)
         form = CommentForm()
+
         comments = Comment.objects.filter(post=post).order_by('-created_on')
 
         context = {
@@ -46,7 +49,6 @@ class PostDetailView(LoginRequiredMixin, View):
         }
 
         return render(request, 'social/post_detail.html', context)
-
     def post(self, request, pk, *args, **kwargs):
         post = Post.objects.get(pk=pk)
         form = CommentForm(request.POST)
@@ -56,7 +58,7 @@ class PostDetailView(LoginRequiredMixin, View):
             new_comment.author = request.user
             new_comment.post = post
             new_comment.save()
-        
+
         comments = Comment.objects.filter(post=post).order_by('-created_on')
 
         context = {
@@ -71,11 +73,11 @@ class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['body']
     template_name = 'social/post_edit.html'
-    
+
     def get_success_url(self):
         pk = self.kwargs['pk']
         return reverse_lazy('post-detail', kwargs={'pk': pk})
-    
+
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
@@ -98,12 +100,8 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return reverse_lazy('post-detail', kwargs={'pk': pk})
 
     def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-
-
-
+        post = self.get_object()
+        return self.request.user == post.author
 
 class ProfileView(View):
     def get(self, request, pk, **kwargs):
@@ -111,17 +109,30 @@ class ProfileView(View):
         user = profile.user
         posts = Post.objects.filter(author=user).order_by('-created_on')
 
+        followers = profile.followers.all()
+
+        if len(followers) == 0:
+            is_following = False
+
+        for follower in followers:
+            if follower == request.user:
+                is_following = True
+                break
+            else:
+                is_following = False
+
+        number_of_followers = len(followers)
+
         context = {
             'user': user,
             'profile': profile,
-            'posts': posts
-                   }
+            'posts': posts,
+            'followers': followers,
+            'number_of_followers': number_of_followers,
+            'is_following': is_following,
+        }
 
         return render(request, 'social/profile.html', context)
-
-
-
-
 
 class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = UserProfile
@@ -135,6 +146,20 @@ class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         profile = self.get_object()
         return self.request.user == profile.user
+
+class AddFollower(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        profile.followers.add(request.user)
+
+        return redirect('profile', pk=profile.pk)
+
+class RemoveFollower(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        profile.followers.remove(request.user)
+
+        return redirect('profile', pk=profile.pk)
 
 
         
